3f9628352e034fff203a2835a06a0c4a3821d488
Nicky31 <naheulbeuk_dwarf_31@hotmail.fr>
2013-05-04 20:21:53 +0200

    J'avais oublié d'add la classe config ...

---
+++ system/core/Config.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * Gestion des configurations
+ */
+
+class ConfigMgr
+{
+    private static $instance = NULL;
+    private        $_loader  = NULL;
+    private        $_configs = array();
+    
+    public static function &instance()
+    {
+        if(!self::$instance)
+            self::$instance = new self;
+        
+        return self::$instance;
+    }
+    
+    private function __construct()
+    {
+        $this->_loader =& Loader::instance();
+    }
+    
+    /*
+     * Note : Les configs.php doivent retourner l'array
+     */
+    public function loadConfig($path,$name)
+    {
+        if(isset($this->_configs[$name]))
+        {
+            throw new Exception('<b>'. __CLASS__ .'</b> : La configuration <b>'. $name .'</b> est déjà chargée !');
+            return FALSE;
+        }
+        
+        $this->_configs[$name] = new Config($this->_loader->getFile($path, TRUE), $name);
+        return $this->_configs[$name];
+    }
+    
+    public function getConfig($name)
+    {
+        if(isset($this->_configs[$name]))
+            return $this->_configs[$name];
+        
+        throw new Exception('<b>'. __CLASS__ .'</b> : Configuration <b>'. $name .'</b> inexistante ou non chargée.');
+    }
+    
+    
+}
+
+class Config implements ArrayAccess
+{
+    private $_content = array();
+    private $_name    = NULL;
+    
+    public function __construct($arrayConfig,$configName)
+    {
+        $this->_content = $arrayConfig;
+        $this->_name = $configName;
+    }
+    
+    public function getItem($k)
+    {
+        if($this->itemExists($k))
+            return $this->_content[$k];
+        
+        throw new Exception('<b>'. __CLASS__ . '</b> : L\'item <b>'. $k .'</b> n\'existe pas dans la configuration <b>'. $this->_name .'</b> !');
+    }
+    
+    public function rewriteItem($k,$v)
+    {
+        $this->_content[$k] = $v;
+    }
+    
+    public function itemExists($k)
+    {
+        return isset($this->_content[$k]);
+    }
+    
+    public function unsetItem($k)
+    {
+        if($this->itemExists($k))
+        {
+            unset($this->_content[k]);
+            return TRUE;
+        }
+        return FALSE;
+    }
+    
+    public function offsetSet($offset, $value)
+    {
+        return $this->rewriteItem($offset, $value);
+    }
+    
+    public function offsetGet($offset)
+    {
+        return $this->getItem($offset);
+    }
+    
+    public function offsetExists($offset)
+    {
+        return $this->itemExists($offset);
+    }
+    
+    public function offsetUnset($offset)
+    {
+        return $this->unsetItem($offset);
+    }
+}
